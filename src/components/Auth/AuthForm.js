53436718dade24066e65ac0a159eef4031146c17
Mostafa <mosi3883@gmail.com>
2022-04-19 17:01:31 +0430

    adding signup

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -13,6 +13,36 @@ const AuthForm = () => {
 
   const submitHandler = (e) => {
     e.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    if (isLogin) {
+    } else {
+      // signup
+      fetch(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyARUDL1Vnfmr6LuYDQGiSsM2jzxf5-bvvU',
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true,
+          }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      ).then((res) => {
+        if (res.ok) {
+        } else {
+          return res.json().then((data) => {
+            //
+            console.log(data);
+          });
+        }
+      });
+    }
   };
 
   return (
@@ -21,11 +51,11 @@ const AuthForm = () => {
       <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input type='email' id='email' ref={emailInputRef} required />
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <input type='password' id='password' ref={passwordInputRef} required />
         </div>
         <div className={classes.actions}>
           <button>{isLogin ? 'Login' : 'Create Account'}</button>
